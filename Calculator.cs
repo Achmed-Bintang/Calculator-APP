f3077ebd187682acd08f0ab07423e7813499868b
Bintang Ganteng <achmedmakkarau@gmail.com>
2024-03-05 12:14:39 +0700

    Sudah

--- Calculator.cs
+++ Calculator.cs
@@ -5,7 +5,22 @@ class Calculator
 		int a = 10;
 		int b = 6;
 		
-		Console.WriteLine("Hasil 
+		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}" , a, b, Penambahan (a, b));
+		Console.WriteLine("Hasil Penambahan: {0} - {1} = {2}" , a, b, Pengurangan (a, b));
+		
+		Console.WriteLine("\nTekan sembarang key untuk keluar");
+		Console.ReadKey();
 	}
-	stat
+	static int Penambahan (int a, int b)
+	{
+		static int Penambahan(int a, int b)
+		{
+			return a + b;
+		}
+
+		static int Pengurangan(int a, int b)
+		{
+			return a - b;
+		}
+	}
 }
